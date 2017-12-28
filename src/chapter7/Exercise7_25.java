d83b11ec530309f8c9ee6b2f0b7e154a377751df
Erlin Goce <erlingoce@gmail.com>
2017-12-28 22:54:40 +0100

    Complete exercise

--- src/chapter7/Exercise7_25.java
+++ src/chapter7/Exercise7_25.java
@@ -1,5 +1,5 @@
 package chapter7;
-
+import java.util.Scanner;
 /**
  * @author pc
  *
@@ -7,7 +7,21 @@ package chapter7;
  */
 public class Exercise7_25 {
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a, b and c: ");
+		double[] eqn = new double[3];
+		double[] roots = new double[3];
+		for (int i = 0; i < eqn.length; i++)
+			eqn[i] = input.nextDouble();
+		int response = solveQuadratic(eqn, roots);
+		if (response == 2) {
+			System.out.println("The roots are " + roots[0] + " and " + roots[1]);
+		}
+		else if (response == 1)
+			System.out.println("The root is " + roots[2]);
+		else
+			System.out.println("The equation has no real solutions");
+		input.close();
 	}
 	public static int solveQuadratic(double[] eqn, double[] roots) {
 		double a = eqn[0];
@@ -15,6 +29,21 @@ public class Exercise7_25 {
 		double c = eqn[2];
 		
 		double discriminant = b * b - 4 * a * c;
-		return 1;
+		
+		if (discriminant > 0) {
+			double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+			double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+			roots[0] = root1;
+			roots[1] = root2;
+			return 2;
+		}
+		else if (discriminant == 0) {
+			double root = (-b / (2 * a));
+			roots[2] = root;
+			return 1;
+		}
+		else
+			return 0;
+		
 	}
 }

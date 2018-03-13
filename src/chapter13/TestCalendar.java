f6bf377d7d93605ebfacee090b09250b654201f2
Erlin Goce <erlingoce@gmail.com>
2018-03-13 13:13:47 +0100

    Change of access of the static members of the class

--- src/chapter13/TestCalendar.java
+++ src/chapter13/TestCalendar.java
@@ -10,19 +10,20 @@ public class TestCalendar {
 		// Construct a Gregorian calendar for the current date and time
 		Calendar calendar = new GregorianCalendar();
 		System.out.println("Current time is " + new Date());
-		System.out.println("YEAR: " + calendar.get(calendar.YEAR));
-		System.out.println("MONTH: " + calendar.get(calendar.MONTH));
-		System.out.println("DATE: " + calendar.get(calendar.DATE));
-		System.out.println("HOUR_OF_DAY: " + calendar.get(calendar.HOUR_OF_DAY));
-		System.out.println("MINUTE: " + calendar.get(calendar.MINUTE));
-		System.out.println("SECOND: " + calendar.get(calendar.SECOND));
-		System.out.println("DAY_OF_YEAR: " + calendar.get(calendar.DAY_OF_YEAR));
-		System.out.println("WEEK_OF_MONTH: " + calendar.get(calendar.WEEK_OF_MONTH));
-		System.out.println("WEEK_OF_YEAR: " + calendar.get(calendar.WEEK_OF_YEAR));
-		System.out.println("YEAR: " + calendar.get(calendar.YEAR));
+		System.out.println("YEAR: " + calendar.get(Calendar.YEAR));
+		System.out.println("MONTH: " + calendar.get(Calendar.MONTH));
+		System.out.println("DATE: " + calendar.get(Calendar.DATE));
+		System.out.println("HOUR_OF_DAY: " + calendar.get(Calendar.HOUR_OF_DAY));
+		System.out.println("MINUTE: " + calendar.get(Calendar.MINUTE));
+		System.out.println("SECOND: " + calendar.get(Calendar.SECOND));
+		System.out.println("DAY_OF_YEAR: " + calendar.get(Calendar.DAY_OF_YEAR));
+		System.out.println("WEEK_OF_MONTH: " + calendar.get(Calendar.WEEK_OF_MONTH));
+		System.out.println("WEEK_OF_YEAR: " + calendar.get(Calendar.WEEK_OF_YEAR));
+		System.out.println("YEAR: " + calendar.get(Calendar.YEAR));
 		
 		// Construct a calendar for 28/10/2015
 		Calendar calendar1 = new GregorianCalendar(2015, 9, 28);
+		
 		String[] dayNameOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday","Thursday",
 				"Friday", "Saturday"};
 		System.out.println("October 28, 2015 is a " +
